f7a23a57c239e9d1f3f81748e64a0930fcbea263
eitanaz <eitan.azriel9@gmail.com>
2015-01-18 23:13:53 +0200

    Assignment 2 solution

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The following 2 functions can be used to return the inversion of a matrix;
+## if the inversion was already computed, it will be fetched from the cache.
 
-## Write a short comment describing this function
-
+## makeCacheMatrix() gets a matrix, and returns a special list that can be used
+## by cachesolve() to return the inversio of the matrix
 makeCacheMatrix <- function(x = matrix()) {
-
+  # Create a variable named 's', that will keep the cached reverse matrix
+  s <- NULL
+  # Now create 4 funcrions that will be used to work with CacheMatrix
+  # The set function should be used whenever we want to set the matrix
+  set <- function(y) {
+    x <<- y
+    # set initializes the solution to NULL (since the matrix was changed,
+    # the solution is not cached anymore)
+    s <<- NULL
+  }
+  # get returns the matrix
+  get <- function() x
+  # setsolve sets the solution
+  setsolve <- function(sol) s <<- sol
+  # getsolve returns the cached solution (NULL if it wasn't set)
+  getsolve <- function() s
+  # This function returns a list of the 4 functions
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## cachesolve() gets a "special" matrix, created by makeCacheMatrix(),
+## and returns its inversion(if it was previously calculated, it's
+## taken from the cache)
+cachesolve <- function(x, ...) {
+  # If the solution is cached, fetch it using getsolve and return it
+  s <- x$getsolve()
+  if(!is.null(s)) {
+    message("getting cached data")
+    return(s)
+  }
+  # Otherwise, fetch the matrix, calculate the solution, save it to the
+  # cache and return it
+  data <- x$get()
+  s <- solve(data, ...)
+  x$setsolve(s)
+  s
 }
